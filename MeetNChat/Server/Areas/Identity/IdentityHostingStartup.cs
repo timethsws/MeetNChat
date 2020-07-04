2d9c640fc8c268902ee80939ab5ead4c272cf2a2
Timeth Subasinghe <thimeth.subasinghe@itelasoft.com.au>
2020-07-04 21:32:50 +0530

    Scafolded Identity

---
+++ MeetNChat/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MeetNChat.Server.Data;
+using MeetNChat.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MeetNChat.Server.Areas.Identity.IdentityHostingStartup))]
+namespace MeetNChat.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
